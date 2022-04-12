eefe00740a406115b6364a2cffaf4601f7659228
marcinde1998 <marcinde1998@gmail.com>
2022-04-12 18:29:34 +0200

    7-1-pobierz-dane-programisty poprawa scryptu

--- 7-1-pobierz-dane-programisty/js/main.js
+++ 7-1-pobierz-dane-programisty/js/main.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     
     $('#pobierz-dane').click(function(){
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data) {
+            
             $( "<div/>", {
                 "id": "dane-programisty",
-                text: ('Imię: ' + data.imie + ', Nazwisko: ' + data.nazwisko + ', Zawód: ' + ', Firma: ' + data.firma)
+                text: ('Imię: ' + data.imie + ', Nazwisko: ' + data.nazwisko + ', Zawód: ' + data.zawod + ', Firma: ' + data.firma)
             }).appendTo( "body" );
+            
         });
     });
 });
